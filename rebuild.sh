3160d9693ac776f8b677ae69fb98da5d662e35a3
Félix Saparelli <felix@passcod.name>
2022-11-15 00:27:07 +1300

    rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -3,9 +3,13 @@
 set -euxo pipefail
 
 npm run all
-git commit -am rebuild
+git commit -am rebuild || true
 
-npm version "${1:-patch}"
+vtarget="${1:-patch}"
+version=$(jq -r .version package.json)
+if [[ "$version" != "$vtarget" ]]; then
+	npm version $vtarget
+fi
 
 version=$(jq -r .version package.json)
 short_tag=$(cut -d. -f1 <<< "$version")
